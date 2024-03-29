c7622c39b4280e2afb82963371d7087e009353ad
Lilian Correia <lilyhttpszzzz@gmail.com>
2024-03-29 01:45:40 -0300

    sétimo commit do App - versão mobile finalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       'orange': '#e28625',
       'green': '#006971',
       'dark-green': '#004140',
-      'offwhite': '#rgba(255, 255, 255, 0.75)',
+      'offwhite': 'rgba(255, 255, 255, 0.75)',
       'white': '#fff',
       'offwhitebg': '#f2f2f2'
     },
